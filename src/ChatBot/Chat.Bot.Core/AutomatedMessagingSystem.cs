a5c6721b780fc19cc5ca5e02cbd013a7b1124854
Thinh Tran <thinhtran@outlook.com.au>
2019-09-07 17:54:42 +0800

    adding test for time equal to delay in minutes

--- src/ChatBot/Chat.Bot.Core/AutomatedMessagingSystem.cs
+++ src/ChatBot/Chat.Bot.Core/AutomatedMessagingSystem.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 
 namespace Chat.Bot.Core
 {
-    public class AutomatedMessagingSystem
+    public class AutomatedMessagingSystem 
     {
         public List<IntervalTriggeredMessage> ManageMessages { get; set; } = new List<IntervalTriggeredMessage>();
         public List<string> QueuedMessages { get; set; } = new List<string>();
+
+    
 
         public void Publish(IntervalTriggeredMessage automatedMessage)
         {
---
+++ src/ChatBot/Chat.Bot.Core/IntervalTriggeredMessage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Chat.Bot.Core
+{
+    public class IntervalTriggeredMessage : IAutomatedMessage
+    {
+        private DateTime _previousRunTimes;
+        public int DelayInMinutes { get; set; }
+        public string Message { get; set; }
+
+        public void Initialize(DateTime currentTime)
+        {
+            _previousRunTimes = currentTime;
+        }
+
+        public bool IsItYourTimeToDisplay(DateTime currentTime)
+        {
+            if (_previousRunTimes.AddMinutes(DelayInMinutes) > currentTime)
+                return false;
+
+            return true;
+        }
+    }
+}
