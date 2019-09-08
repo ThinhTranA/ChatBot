17bb592a63d46f2ec0b8ca7ec92a7fdccc1c4e39
Thinh Tran <thinhtran@outlook.com.au>
2019-09-08 09:06:24 +0800

    Adding simple running of the bot

--- src/ChatBot/Chat.Bot/Program.cs
+++ src/ChatBot/Chat.Bot/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Chat.Bot.Core;
+using System;
+using System.Threading;
+
 
 namespace ChatBot
 {
@@ -6,7 +9,22 @@ namespace ChatBot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Initializing the Bot...");
+            Console.WriteLine("To exit, press [Ctrl]+C...");
+            var automatedMessagingSystem = new AutomatedMessagingSystem();
+            automatedMessagingSystem.Publish(new IntervalTriggeredMessage { DelayInMinutes = 1, Message = "Hello, everyone! I am the bot!" });
+           
+
+            while (true)
+            {
+                Thread.Sleep(1000);
+                automatedMessagingSystem.CheckMessages(DateTime.Now);
+             
+                while(automatedMessagingSystem.DequeueMessage(out string theMessage))
+                {
+                    Console.WriteLine($"{DateTime.Now.ToShortTimeString()} - {theMessage}");
+                }
+            }
         }
     }
 }
